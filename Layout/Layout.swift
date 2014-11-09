c7e5f10f022a3b0ebf3620e46ac1389ae0fe296f
Santosh Rajan <santrajan@gmail.com>
2014-11-09 09:28:26 +0530

    Version 0.1.0

---
+++ Layout/Layout.swift
@@ -0,0 +1,126 @@
+//
+//  Layout.swift
+//  Layout
+//
+//  Created by Santosh Rajan on 09/11/14.
+//  Copyright (c) 2014 SantoshRajan. All rights reserved.
+//
+
+
+import UIKit
+
+
+class HorizontalLayout: UIView {
+    
+    var xOffsets: [CGFloat] = []
+    
+    init(height: CGFloat) {
+        super.init(frame: CGRectMake(0, 0, 0, height))
+    }
+    
+    required init(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func layoutSubviews() {
+        
+        var width: CGFloat = 0
+        
+        for i in 0..<subviews.count {
+            var view = subviews[i] as UIView
+            view.layoutSubviews()
+            width += xOffsets[i]
+            view.frame.origin.x = width
+            width += view.frame.width
+        }
+        
+        self.frame.size.width = width
+        
+    }
+    
+    override func addSubview(view: UIView) {
+        
+        xOffsets.append(view.frame.origin.x)
+        super.addSubview(view)
+        
+    }
+    
+    func removeAll() {
+        
+        for view in subviews {
+            view.removeFromSuperview()
+        }
+        xOffsets.removeAll(keepCapacity: false)
+        
+    }
+    
+}
+
+
+class VerticalLayout: UIView {
+    
+    var yOffsets: [CGFloat] = []
+    
+    init(width: CGFloat) {
+        super.init(frame: CGRectMake(0, 0, width, 0))
+    }
+    
+    required init(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func layoutSubviews() {
+        
+        var height: CGFloat = 0
+        
+        for i in 0..<subviews.count {
+            var view = subviews[i] as UIView
+            view.layoutSubviews()
+            height += yOffsets[i]
+            view.frame.origin.y = height
+            height += view.frame.height
+        }
+        
+        self.frame.size.height = height
+        
+    }
+    
+    override func addSubview(view: UIView) {
+        
+        yOffsets.append(view.frame.origin.y)
+        super.addSubview(view)
+        
+    }
+    
+    func removeAll() {
+        
+        for view in subviews {
+            view.removeFromSuperview()
+        }
+        yOffsets.removeAll(keepCapacity: false)
+        
+    }
+    
+}
+
+class VerticalScreenLayout: VerticalLayout {
+    
+    
+    init() {
+        super.init(width: UIScreen.mainScreen().bounds.width)
+    }
+    
+    required init(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func layoutSubviews() {
+        
+        self.frame.size.width = UIScreen.mainScreen().bounds.width
+        super.layoutSubviews()
+        
+    }
+    
+}
+
+
---
+++ README.md
@@ -0,0 +1,7 @@
+# Layout.swift
+
+## Dynamic Layouts for Swift
+
+To Install, just include the file Layout.swift in your project.
+
+More info in this article [Dynamic Layout in Swift](https://medium.com/swift-programming/dynamic-layouts-in-swift-b56cf8049b08)
