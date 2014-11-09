aa5d5e1b543decdfcc0375e3fc0254f97cc38baf
Santosh Rajan <santrajan@gmail.com>
2014-11-09 10:58:36 +0530

    Version 0.1.2

--- Layout/Layout.swift
+++ Layout/Layout.swift
@@ -103,6 +103,7 @@ class VerticalLayout: UIView {
     
 }
 
+
 class VerticalScreenLayout: VerticalLayout {
     
     
@@ -123,4 +124,40 @@ class VerticalScreenLayout: VerticalLayout {
     
 }
 
+
+class VerticalFitLayout: VerticalLayout {
+    
+    
+    override init(width: CGFloat) {
+        super.init(width: width)
+    }
+    
+    required init(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func layoutSubviews() {
+        
+        var height: CGFloat = 0
+        var zeroHeightView: UIView?
+        
+        for i in 0..<subviews.count {
+            var view = subviews[i] as UIView
+            height += yOffsets[i]
+            if view.frame.height == 0 {
+                zeroHeightView = view
+            } else {
+                height += view.frame.height
+            }
+        }
+        
+        if height < superview!.frame.height && zeroHeightView != nil {
+            zeroHeightView!.frame.size.height = superview!.frame.height - height
+        }
+        
+        super.layoutSubviews()
+        
+    }
+    
+}
 
