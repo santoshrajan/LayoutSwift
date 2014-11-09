7547355605355590560d7069286053824a5bef39
Santosh Rajan <santrajan@gmail.com>
2014-11-09 09:12:55 +0530

    Initial Commit

---
+++ Layout/Layout.h
@@ -0,0 +1,19 @@
+//
+//  Layout.h
+//  Layout
+//
+//  Created by Santosh Rajan on 09/11/14.
+//  Copyright (c) 2014 SantoshRajan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Layout.
+FOUNDATION_EXPORT double LayoutVersionNumber;
+
+//! Project version string for Layout.
+FOUNDATION_EXPORT const unsigned char LayoutVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Layout/PublicHeader.h>
+
+
